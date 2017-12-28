9e29d48f6ddcb0464ac9816b5a56862d960c1780
Shivashankar Reddy <shiva.kashireddy@gmail.com>
2017-12-28 17:31:59 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { LoginDetailsService } from './service/login-details.service';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,    
+    RouterModule.forRoot([
+      { path: "loginpage" , component: LoginComponent },
+      { path: "homepage" ,component: HomepageComponent },
+      { path: "" , redirectTo: "/loginpage" , pathMatch:"full" },
+      { path: "**" , redirectTo: "/loginpage" , pathMatch:"full" },
+    ])
   ],
-  providers: [],
+  providers: [LoginDetailsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
