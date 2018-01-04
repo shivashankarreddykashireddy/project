426841120b47d5dc6f50d3c24d115e762ab674b5
Shivashankar Reddy <shiva.kashireddy@gmail.com>
2018-01-04 10:31:00 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,8 +12,6 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   myData = {};
   data11 : any;
-  username1 : any;
-  password1: any;
   constructor(private loginservice: LoginDetailsService , private router: Router ) { 
     this.loginservice.loginData().subscribe(res => this.data11 = res, err => console.log(err));             
   }
@@ -22,15 +20,10 @@ datasubmit(myData) {
   console.log('data --', myData);
   console.log('username is --', myData.username);
   console.log('password is --', myData.password);
-  console.log("11111111111" , this.username1);
-  // console.log('this.data11 --', this.data11.username);
-
-  if(myData.username == this.data11.username && myData.password == this.data11.password ){
+  
+  if(myData.username == this.data11[0].username && myData.password == this.data11[0].password ){
       this.router.navigate(['/homepage']);
     }
-    // if(myData.username == "" && myData.password == ""){      
-    //   alert("Enter Data !");      
-    // }
     else {
       alert("Wrong Data Entered !");
     }
